22f3a7e369781e7ea49c3edbcbfbb65b4598c9e8
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-08 11:14:45 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
+//Selects all panesl
+
 const panels = document.querySelectorAll('.panel');
 
+
+//Function to add class of open to clicked panel
 function toggleOpen() {
     this.classList.toggle('open');
 }
 
+
+//Function to add class of open-active to open panel once transition ends
 function toggleActive(e) {
     if(e.propertyName.includes('flex')) {
     this.classList.toggle('open-active')   
     }
 }
+
+//Event listeners on panels
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
